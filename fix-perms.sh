21e21f18410a346a849b128e332a4a947420474f
Xenon <nobelvansh118@gmail.com>
2026-07-05 17:28:30 +0530

    Update fix-perms.sh

--- fix-perms.sh
+++ fix-perms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /data/.openclaw
 chown -R node:node /data
-exec su node -c "node dist/index.js gateway --bind lan --port 8080 --allow-unconfigured --controlui-allowed-origins https://ai.roxco.xyz"
+exec su node -c "node dist/index.js gateway --bind lan --port 8080 --allow-unconfigured "
